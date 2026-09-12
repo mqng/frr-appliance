44290966aafd8aa706dbaebe7c37db42e1b46098
Minh Nguyen <minh.nguyen@crealogix.com>
2026-09-12 17:44:36 +0200

    .

--- ci/finalize-artifacts.sh
+++ ci/finalize-artifacts.sh
@@ -37,6 +37,8 @@ qcow_sha=$(sha256sum "out/$name.qcow2" | awk '{print $1}')
 iso_sha=$(sha256sum "out/$name-installer.iso" | awk '{print $1}')
 packages_sha=$(sha256sum "out/$name-packages.txt" | awk '{print $1}')
 source_json=$(cat out/base-source.json)
+base_iso_url=$(jq -r '.installer_iso_source + "/" + .installer_iso' out/base-source.json)
+base_iso_sha512=$(jq -r '.installer_iso_sha512' out/base-source.json)
 
 jq -n \
   --arg variant "$variant" --arg build_time "$APPLIANCE_BUILD_TIME" \
@@ -50,7 +52,7 @@ jq -n \
 
 jq -n \
   --arg subject "$name" --arg commit "${CI_COMMIT_SHA:-local}" --arg repo "${CI_PROJECT_URL:-local}" --arg pipeline "${CI_PIPELINE_URL:-local}" \
-  --arg base_url "$DEBIAN_BASE_URL/$DEBIAN_ISO_NAME" --arg base_sha512 "$DEBIAN_ISO_SHA512" \
+  --arg base_url "$base_iso_url" --arg base_sha512 "$base_iso_sha512" \
   --arg frr_key "$FRR_KEYRING_SHA256" --arg vpp_key "$VPP_KEYRING_SHA256" \
   --arg img "$img_sha" --arg zst "$zst_sha" --arg qcow "$qcow_sha" --arg iso "$iso_sha" --arg packages "$packages_sha" \
   '{_type:"https://in-toto.io/Statement/v1",subject:[{name:($subject+".img"),digest:{sha256:$img}},{name:($subject+".img.zst"),digest:{sha256:$zst}},{name:($subject+".qcow2"),digest:{sha256:$qcow}},{name:($subject+"-installer.iso"),digest:{sha256:$iso}}],predicateType:"https://slsa.dev/provenance/v1",predicate:{buildDefinition:{buildType:"https://gitlab.com/frr-appliance/mmdebstrap-loop@v3",externalParameters:{source_repository:$repo,source_commit:$commit},resolvedDependencies:([{uri:$base_url,digest:{sha512:$base_sha512}},{uri:"https://deb.frrouting.org/frr/keys.gpg",digest:{sha256:$frr_key}},{uri:"urn:frr-appliance:installed-packages",digest:{sha256:$packages}}] + (if $vpp_key != "none" then [{uri:"https://packagecloud.io/fdio/release/gpgkey",digest:{sha256:$vpp_key}}] else [] end))},runDetails:{builder:{id:$pipeline},metadata:{invocationId:$pipeline}}}}' \
